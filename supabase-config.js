b02024438f3fac1c65ecd71cb8eb0c631f1168f0
vissgaib-dev <vissgaib@gmail.com>
2026-07-10 16:22:12 +0800

    Create supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://xbfucugzuuxjnmyevpzp.supabase.co";
+const SUPABASE_KEY = "sb_publishable_94wBQuncLA76BsuWGNqtlw_sBPbgj39";
+
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
